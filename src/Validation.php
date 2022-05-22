e5d49ff05d0c5992ec2e345a1a93652eacb81d71
ShzRebot <shakibzeidi317@gmail.com>
2022-05-22 14:55:28 +0430

    bug fixed

--- src/Validation.php
+++ src/Validation.php
@@ -1,15 +1,15 @@
 <?php namespace RoocketValidation;
 
-class RoocketValidation
+class Validation
 {
     public function has(array $data , string $field)
     {
-        // return isset($data[$field]); 
+        return isset($data[$field]); 
     }
 
     public function get()
     {
-        
+        return 'get data';
     }
 
     public function set()
